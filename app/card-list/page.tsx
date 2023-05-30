8528a950d1783a30f6d29c13016e9df53259cdb0
Ankush Bansal <40947981+ankushbansal2000@users.noreply.github.com>
2023-05-30 19:35:34 +0530

    page

--- app/card-list/page.tsx
+++ app/card-list/page.tsx
@@ -6,12 +6,14 @@ import styles from "./../css/main.module.css";
 export default function CardList() {
     const CardProps = [
         {
+            id: 0,
         title: "i am a title",
         description: "I am a description1",
         buttonText: "Button text",
         imageUrl: "Image url"
       },
       {
+        id: 1,
         title: "i am a title2",
         description: "I am a description2",
         buttonText: "Button text"
@@ -24,7 +26,7 @@ export default function CardList() {
           <Container>
             <div className={styles.Card_List}>  
           {CardProps.map((elem, index) => 
-          <Card {...CardProps[index]}></Card>
+          <Card key={elem.id} {...CardProps[index]}></Card>
           )}
           </div>
           </Container>
